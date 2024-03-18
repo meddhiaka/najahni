dc5a0527847627029cf7e741db522b8354f6f090
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-03-18 04:20:25 +0100

    code: cors added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,9 @@
 import express, { Express, Request, Response } from 'express'
 import router from './router'
+import cors from 'cors'
 
 const app: Express = express()
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
