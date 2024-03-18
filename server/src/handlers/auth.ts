f0f930809ae439e2786e38ed27890c1e52dee36c
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-03-18 04:20:49 +0100

    code: decryption module for sign in logic

--- server/src/handlers/auth.ts
+++ server/src/handlers/auth.ts
@@ -1,11 +1,16 @@
 import { prisma } from "../lib/prisma"
-import { hashPassword } from "./create"
 import { Request, Response } from "express"
+import bcrypt from "bcrypt"
 import validator from "validator"
+
+async function decryptPassword(password: string, cryptedPassword: string): Promise<boolean> {
+    const b: boolean = await bcrypt.compare(password, cryptedPassword);
+    return b
+}
 
 async function loginUser(req: Request, res: Response) {
     const { email, password }: { email: string, password: string } = req.body
-    if (validator.isAlphanumeric(email) || validator.isAlphanumeric(password)) return res.status(400).json({ msg: "invalid input" })
+    // if (validator.isAlphanumeric(email) || validator.isAlphanumeric(password)) return res.status(400).json({ msg: "invalid input" })
 
     try {
         const user = await prisma.user.findUnique({
@@ -18,9 +23,9 @@ async function loginUser(req: Request, res: Response) {
                 image: true
             }
         })
+
         if (user) {
-            const testPass: string = await hashPassword(password)
-            if (user.password === testPass) return res.status(200).json({ user })
+            await decryptPassword(password, user.password) ? res.status(200).json(user) : res.status(401).json({ msg: "matnajemech!" })
         }
     } catch (e) {
         console.error("src/handlers/auth.ts:\n", e)
