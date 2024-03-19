310b8fffcf658075ee161a7c188d3b2c21a85e2f
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-03-19 05:07:43 +0100

    code: serverside user handler added

---
+++ server/src/handlers/user.ts
@@ -0,0 +1,45 @@
+import { prisma } from "../lib/prisma";
+import { Request, Response } from "express";
+import validator from "validator"
+
+interface uniqueUser {
+    id: string
+    email: string
+    name: string
+}
+
+
+async function findUser(email: string): Promise<uniqueUser> {
+    const user: uniqueUser = await prisma.user.findUnique({
+        where: {
+            email: email
+        },
+        select: {
+            id: true,
+            email: true,
+            name: true
+        }
+    })
+    return user
+}
+
+async function getUser(req: Request, res: Response) {
+
+    const email: string = req.body.email
+
+    if (!validator.isEmail(email)) return res.status(404).json({ msg: "should be an adress" })
+
+    const user: uniqueUser = await findUser(email)
+
+    if (user) {
+        res.status(200).json(user)
+    } else {
+        res.status(404).json(null)
+    }
+
+}
+
+export {
+    getUser,
+    findUser
+}
