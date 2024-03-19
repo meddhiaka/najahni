b2e16f9b8aac4fb12ed61515085cf9503b7cd2d1
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-03-19 05:08:51 +0100

    code: serverside user and demoUser controllers added

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { createUser } from "./handlers/create";
 import { loginUser } from "./handlers/auth";
+import { getUser } from "./handlers/user";
+import { createDemoUser } from "./handlers/demoUser";
 
 const router: Router = Router()
 
 router.post("/signin", loginUser)
 router.post("/signup", createUser)
-router.post('/',)
+router.post("/user", getUser)
+router.post("/oauth", createDemoUser)
 
 export default router
