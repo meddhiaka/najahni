336612d86f4119609dd0958142db7007dd1cc466
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-03-21 00:57:13 +0100

    code: added next auth provider

--- client/src/app/context/NextAuthProvider.tsx
+++ client/src/app/context/NextAuthProvider.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export function NextAuthProvider(
     { children }: { children: React.ReactNode }
 ) {
+
     return (
-        <SessionProvider>
+        <SessionProvider >
             {children}
         </SessionProvider>
     )
