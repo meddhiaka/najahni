c977b2566a29bbf79799d8d247e175c8acab1bd2
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-03-31 07:49:49 +0100

    code: added upload route (for testing)

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,6 +3,7 @@ import { createUser } from "./handlers/create";
 import { loginUser } from "./handlers/auth";
 import { getUser } from "./handlers/user";
 import { createDemoUser } from "./handlers/demoUser";
+import { uploadImage } from "./handlers/upload";
 
 const router: Router = Router()
 
@@ -10,5 +11,8 @@ router.post("/signin", loginUser)
 router.post("/signup", createUser)
 router.post("/user", getUser)
 router.post("/oauth", createDemoUser)
+router.post("/upload", uploadImage.single('pdp'), async (req, res) => {
+    res.json({ message: "image is uploaded" })
+})
 
 export default router
