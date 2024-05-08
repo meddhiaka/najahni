b8ba2fd1b50865d41c079640d1e228a3b663c8a2
mohamed <mahemedmegat@gmail.com>
2024-05-08 16:14:58 +0100

    fix: formatted decrypt password function in auth module

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -1,9 +1,12 @@
 import bcrypt from "bcrypt"
 import validator from "validator"
 
-async function decryptPassword(password: string, cryptedPassword: string): Promise<boolean> {
-    const b: boolean = await bcrypt.compare(password, cryptedPassword);
-    return b
+async function decryptPassword(password: string, cryptedPassword: string | null): Promise<boolean> {
+    if (cryptedPassword) {
+        const b: boolean = await bcrypt.compare(password, cryptedPassword);
+        return b
+    }
+    return false;
 }
 
 async function hashPassword(str: string): Promise<string> {
