eca6b4da1cf538ee58bf966575e2a540b6a106c3
mohamed <mahemedmegat@gmail.com>
2024-05-08 16:16:35 +0100

    fix: removed client side functionalities from next auth provider

--- client/src/context/NextAuthProvider.tsx
+++ client/src/context/NextAuthProvider.tsx
@@ -1,29 +1,13 @@
 "use client";
 import { SessionProvider, useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
 
-export  function NextAuthProvider(
+export function NextAuthProvider(
     { children }: { children: React.ReactNode }
 ) {
-    const [sessionObject, setSessionObject] = useState(null)
-    useEffect(() => {
-        async function fetchSession() {
-            const { session } = useSession()
-            setSessionObject(session)
-            console.log(sessionObject)
-        }
-
-        fetchSession()
-
-    }, [])
+    
     return (
-        <SessionProvider value={{sessionObject}}>
+        <SessionProvider >
             {children}
         </SessionProvider>
     )
-}
-
-
-export default function ConsumeSession() {
-
 }
