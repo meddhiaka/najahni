2db8df5d4bf4d38eb25c99655d5c54d0640781f1
mohamed <mahemedmegat@gmail.com>
2024-05-08 16:20:03 +0100

    code: implemented specific profile section

--- client/src/app/home/profile/[id]/page.tsx
+++ client/src/app/home/profile/[id]/page.tsx
@@ -1,9 +1,8 @@
-'use client'
 import { prisma } from "@/lib/prisma"
-import { usePathname } from "next/navigation"
-import { useEffect } from "react"
-import { AlertDestructive } from "@/components/AlertEx"
-import { DialogDemo } from "@/components/EditProfileEx"
+import Image from "next/image"
+import img from "./../../../../../public/avatar.jpg"
+import { SheetDemo } from "@/components/ProfileFormSlider"
+import { CourDescription } from "@/components/CourDescription"
 
 function getId(a: string): string {
     const tab = a.split("/")
@@ -11,16 +10,39 @@ function getId(a: string): string {
     return res
 }
 
-export default function Profile({ params }: { params: { slug: string } }) {
+async function getProfileData(id: string) {
+    const data = await prisma.user.findUnique({
+        where: {
+            id: id
+        },
+        select: {
+            name: true,
+            role: true,
+            email: true,
+            description: true
+        }
+    })
+    return data
+}
+
+export default async function Profile({ params }: { params: { id: string } }) {
+    const e = await getProfileData(params.id)
     return (
-        <div className="max-w-5xl mx-auto px-4 space-y-7">
-            <div className="flex space-x-2">
-                <h1 className="text-4xl font-semibold">Profile</h1>
-                <DialogDemo />
+        <div className="mx-auto space-y-7">
+            <div className="p-28 bg-gradient-to-r from-white  via-red-300 to-violet-500 relative">
+                <div className="top-28 md:top-20  shadow-md shadow-zinc-400 w-32 h-32 md:w-40 md:h-40 left-5 md:left-16 bg-black absolute rounded-md border-[.3rem] border-violet-100">
+                    <Image src={img} alt="" width={100} height={100} className="w-full" />
+                </div>
+                <p className="absolute text-black bottom-7 left-36 md:left-56 mx-2 text-2xl md:text-3xl font-medium">{e?.name}</p>
+                <p className="absolute text-slate-500 bottom-1 left-36 md:left-56 mx-2 text-base font-semibold">{e?.role}</p>
+                <div className="top-1 right-1 absolute">
+                    <SheetDemo email={e?.email} name={e?.name} description={e?.description} />
+                </div>
             </div>
-            <div className="max-w-md">
-                <AlertDestructive />
+            <div className="py-4">
+                <CourDescription nomCour="À propos" description={e?.description} />
             </div>
+
         </div>
     )
 }
