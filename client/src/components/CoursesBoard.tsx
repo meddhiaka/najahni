76e0603a49560a3de67ca7514bc2597cd9b8afe3
mohamed <mahemedmegat@gmail.com>
2024-05-08 16:32:47 +0100

    code: implemented demo course board ui

--- client/src/components/CoursesBoard.tsx
+++ client/src/components/CoursesBoard.tsx
@@ -2,8 +2,8 @@ import { prisma } from "@/lib/prisma"
 import { CardWithForm } from "./CourCard"
 import { $Enums } from "@prisma/client";
 
-async function findCoursesOfUser({id}:{ id: string | undefined; }) {
-    if(id) {
+async function findCoursesOfUser({ id }: { id: string | undefined; }) {
+    if (id) {
         const courses = prisma.user.findUnique({
             where: {
                 id: id
@@ -12,25 +12,29 @@ async function findCoursesOfUser({id}:{ id: string | undefined; }) {
                 cours: true
             }
         })
-        console.log(courses)
         return courses
     }
 }
 
-export async function CoursesBoard(id:{ id: string | undefined; }) {
+export async function CoursesBoard(id: { id: string | undefined; }) {
     const courses = await findCoursesOfUser(id)
-    console.log(courses)
     return (
-        <div className="flex flex-row flex-wrap gap-4 max-w-full mx-auto">
+        <div className="flex flex-row flex-wrap gap-4 max-w-full mx-auto min-h-[3000px]">
             {
-                courses?.cours.map(e => (
-                    <CardWithForm 
-                        name={e.name}
-                        description={e.description} 
-                        id={e.id} 
-                        section={e.section} 
-                    />
-                ))
+                courses?.cours.length !== 0 ? (
+                    courses?.cours.map(e => (
+                        <CardWithForm
+                            name={e.name}
+                            description={e.description}
+                            id={e.id}
+                            section={e.section}
+                        />
+                    ))
+                ) : (
+                    <div className="max-w-5xl mx-auto">
+                        Il n'y a pas de cours ; vous devez en télécharger.
+                    </div>
+                )
             }
         </div>
     )
