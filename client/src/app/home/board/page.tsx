28e5a79e3a3ae2f04cca4beaed6298f1c2fc77ab
Med Dhia Kassab <mohameddhiakb@gmail.com>
2024-07-01 12:44:22 +0100

    code: user serverside get function with role & id props

--- client/src/app/home/board/page.tsx
+++ client/src/app/home/board/page.tsx
@@ -4,6 +4,8 @@ import { CoursesBoard } from "@/components/CoursesBoard"
 import { AlertDialogDemo } from "@/components/DialogCour"
 import { prisma } from "@/lib/prisma"
 import { getServerSession } from "next-auth"
+import { RedirectType, redirect } from "next/navigation"
+
 
 async function getSessionCustom() {
   const response = await getServerSession(authOptions)
@@ -17,7 +19,8 @@ async function getUserId(email: string | null | undefined) {
         email: email
       },
       select: {
-        id: true
+        id: true,
+        role: true
       }
     })
     return res
@@ -29,16 +32,23 @@ async function getUserId(email: string | null | undefined) {
 export default async function PersonalBoard() {
   const session = await getSessionCustom()
   const idData = await getUserId(session?.user?.email)
+  const role = idData?.role
   return (
     <div className="w-screen  border-t pt-4">
-      <div className="mx-auto max-w-7xl">
-        <div className="flex justify-end">
-          <AlertDialogDemo id={idData} />
-        </div>
-        <div className="pt-6">
-         <CoursesBoard id={idData?.id} />
-        </div>
-      </div>
+      {
+        role == "TEACHER" ? (
+          <div className="mx-auto max-w-7xl">
+            <div className="flex justify-end">
+              <AlertDialogDemo id={idData} />
+            </div>
+            <div className="pt-6">
+              <CoursesBoard id={idData?.id} />
+            </div>
+          </div>
+        ) : (
+          <div className="text-center text-4xl grid place-items-center h-[70vh]">UNAUTHORIZED</div>
+        )
+      }
     </div>
   )
 }
